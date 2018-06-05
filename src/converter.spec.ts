3c43cf6820a37ead5c941b05d8c880b2cc57f616
RBLU <reto.blunschi@youpers.com>
2018-06-05 10:46:49 +0200

    fixed angular tests

---
+++ src/converter.spec.ts
@@ -0,0 +1,13 @@
+import {convert} from './converter';
+
+describe('converter should', () => {
+
+  describe('convert lenghts', () => {
+
+  });
+
+  describe('convert weights', () => {
+
+  });
+
+});
